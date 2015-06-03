d94a60faa8ec545a95634ead17aa39c09163e404
rob gallagher <rob.gallagher@gmail.com>
2015-06-03 15:26:14 +0100

    Key id should be a full fingerprint

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -6,6 +6,9 @@ class percona::repo {
         location   => 'http://repo.percona.com/apt',
         release    => $::lsbdistcodename,
         repos      => 'main',
-        id         => '1C4CBDCDCD2EFD2A',
+        key        => {
+          'id'     => '430BDF5C56E7C94E848EE60C1C4CBDCDCD2EFD2A',
+          'server' => 'subkeys.pgp.net',
+        },
     }
 }
